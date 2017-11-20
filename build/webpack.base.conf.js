e3b5be2d44f23fb64c13769063a6e498893bca79
skye <forbetter@yeah.net>
2017-11-20 23:59:36 +0800

    add typeScript

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,7 +10,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.ts'
   },
   output: {
     path: config.build.assetsRoot,
@@ -20,7 +20,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.ts'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
@@ -35,6 +35,20 @@ module.exports = {
         include: [resolve('src'), resolve('test')],
         options: {
           formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: '/node_modules/',
+        enforce: 'pre',
+        loader: 'tslint-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: '/node_modules/',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
         }
       },
       {
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import axios from 'axios'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+axios.interceptors.request.use((config) => {
+  config.headers.access_token = localStorage.getItem('access_token') || ''
+  return config
+})
+
+Vue.use(iView)
+
+Vue.config.productionTip = false
+
+const whiteList = ['/login']
+
+router.beforeEach((to, from, next) => {
+  if (store.state.user.token) {
+    next()
+  } else if (whiteList.indexOf(to.path) !== -1) {
+    // 在免登录白名单，直接进入
+    next()
+  } else {
+    // 否则全部重定向到登录页
+    next({ path: '/login' })
+  }
+})
+
+/* eslint-disable no-new */
+const v = new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<app></app>',
+  components: { App }
+})
