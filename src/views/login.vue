2fc1e7af67fc2daeda90d016bf50f163f29c92c6
SkyeTang <dengjc@ecworking.com>
2017-12-07 10:31:55 +0800

    edit login.

--- src/views/login.vue
+++ src/views/login.vue
@@ -18,10 +18,10 @@
   import Axios from 'axios';
   import Singin from './components/login/singin';
   import Register from './components/login/register';
-  import bg1 from '../assets/images/bg1.jpg';
-  import bg2 from '../assets/images/bg2.jpg';
+  import bg1 from '../assets/images/bg1.jpeg';
+  import bg2 from '../assets/images/bg2.jpeg';
   import bg3 from '../assets/images/bg3.jpg';
-  import bg4 from '../assets/images/bg4.jpg';
+  import bg4 from '../assets/images/bg4.jpeg';
 
   export default {
     components: {
